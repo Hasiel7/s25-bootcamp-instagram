573ce792c7b6e39024b9f54ae3876c0affc9ca60
Jahaziel Herberth <hasiel@ad.unc.edu>
2025-02-09 21:53:40 -0500

    finished product

--- instagram demo/instagram demo/PostView.swift
+++ instagram demo/instagram demo/PostView.swift
@@ -9,8 +9,61 @@ import SwiftUI
 
 struct PostView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        VStack{
+                  Text("____________________________________________")
+                      .foregroundColor(.secondary)
+                      .padding(.vertical, 8)
+                  HStack{
+                      Image(systemName: "person.crop.circle")
+                      Text("Itsjahh3")
+                          .font(.subheadline)
+                          .fontWeight(.bold)
+                      Spacer()
+                      Text("...")
+                  }
+                  .padding(.horizontal, 8)
+                  Image("pic")
+                      .resizable()
+                      .scaledToFit()
+                  HStack{
+                      Image(systemName: "heart")
+                      Image(systemName: "message")
+                      Image(systemName: "paperplane")
+                      Spacer()
+                      Image(systemName: "bookmark")
+                      
+                  }
+                  .padding(.horizontal, 16)
+                  .padding(.vertical, 2)
+                  
+                  HStack{
+                      Text("456,920 likes")
+                          .font(.footnote)
+                          .fontWeight(.semibold)
+                          .frame(maxWidth: .infinity, alignment: .leading)
+                                      
+                  }
+                  .padding(.horizontal, 16)
+                  .padding(.vertical, 2)
+                  HStack{
+                      Text("Itsjahh3 ")
+                          .font(.footnote)
+                          .fontWeight(.semibold) +
+                      Text("Lorem ipsum odor amet, consectetuer adipiscing elit. Est amet a dictum blandit suscipit class finibus, justo neque.")
+                          .font(.footnote)
+                  }
+                  .padding(.horizontal,8)
+                  Text("February 9")
+                      .font(.caption)
+                      .foregroundColor(.secondary)
+                      .frame(maxWidth: .infinity, alignment: .leading)
+                      .padding(.horizontal, 11)
+                      .padding(.vertical, 2)
+              }
+              Text("____________________________________________")
+                  .foregroundColor(.secondary)
+          }
+
 }
 
 #Preview {
