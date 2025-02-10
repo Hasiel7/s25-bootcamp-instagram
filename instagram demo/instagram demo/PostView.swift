b10377521ac132ddf9f1a11b7fe0b1ef820774a1
Jahaziel Herberth <hasiel@ad.unc.edu>
2025-02-09 21:50:56 -0500

    Instagram demo

---
+++ instagram demo/instagram demo/PostView.swift
@@ -0,0 +1,18 @@
+//
+//  PostView.swift
+//  instagram demo
+//
+//  Created by Jahaziel Herberth Martinez on 2/9/25.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostView()
+}
---
+++ instagram demo/instagram demo/instagram_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_demoApp.swift
+//  instagram demo
+//
+//  Created by Jahaziel Herberth Martinez on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct instagram_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
